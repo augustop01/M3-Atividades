7c944e3a5d9065691f6cb0739a5f841ecdceea87
augustop01 <mpires.augusto@gmail.com>
2024-03-16 01:28:23 -0300

    feat: add simulation for notification by phone number

---
+++ m3s06/src/main/java/br/senai/sc/m3s06/operations/notification/SMSNotifications.java
@@ -0,0 +1,15 @@
+package br.senai.sc.m3s06.operations.notification;
+
+import br.senai.sc.m3s06.interfaces.Notifications;
+import br.senai.sc.m3s06.model.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SMSNotifications implements Notifications {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(EmailNotifications.class);
+    @Override
+    public void send(Person person, String content) {
+        LOGGER.info("Notification for {}: {}", person.getPhone(), content);
+    }
+}
